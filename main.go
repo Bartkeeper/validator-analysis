25af13bd36dcd5b02e458688c1f5c973502368ba
mscdr <martin.schaeffner@datarella.com>
2022-12-03 18:22:14 +0100

    challenge_01

---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	staking "github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+type valAdd struct {
+	moniker         string
+	opAdd           string
+	accAdd          string
+	votingpower     string
+	selfDelegation  string
+	totalDelegation string
+}
+
+var valAdds []valAdd
+
+func main() {
+	var grpcConn *grpc.ClientConn
+	grpcConn, _ = getGrpcConn()
+	var vals, _ = getVals(grpcConn)
+	var valAddresses = getValAddresses(vals)
+	getSelfDelegation(grpcConn, valAddresses)
+
+	defer grpcConn.Close()
+	exportCSV(valAdds)
+}
+
+func getGrpcConn() (*grpc.ClientConn, error) {
+	grpcConn, err := grpc.Dial(
+		"cosmos-grpc.polkachu.com:14990", // Polkachu's gRPC server address.
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultCallOptions(grpc.ForceCodec(codec.NewProtoCodec(nil).GRPCCodec())),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return grpcConn, nil
+}
+
+func getVals(grpcConn *grpc.ClientConn) ([]staking.Validator, error) {
+
+	// This creates a gRPC client to query the x/staking service.
+	stakingClient := staking.NewQueryClient(grpcConn)
+	// bankClient := bank.NewQueryClient(grpcConn)
+	stakingRes2, err := stakingClient.Validators(
+		context.Background(),
+		&staking.QueryValidatorsRequest{Status: "BOND_STATUS_BONDED"},
+	)
+	if err != nil {
+		return nil, err
+	}
+	vals := stakingRes2.GetValidators()
+
+	return vals, nil
+}
+
+func getTotalTokens(vals []staking.Validator) int64 {
+	var total int64
+	for _, val := range vals {
+		total = total + val.GetBondedTokens().BigInt().Int64()
+	}
+
+	return total
+}
+
+func getVotingPower(val staking.Validator, totalTokens int64) float64 {
+	var vp = float64(val.BondedTokens().Int64()) / float64(totalTokens)
+	return vp
+}
+
+func deriveValAccAddress(val staking.Validator) (sdk.AccAddress, error) {
+	valAddr, err := sdk.ValAddressFromBech32(val.OperatorAddress)
+	if err != nil {
+		return nil, err
+	}
+	accAddr, err := sdk.AccAddressFromHexUnsafe(hex.EncodeToString(valAddr.Bytes()))
+	if err != nil {
+		return nil, err
+	}
+	return accAddr, nil
+}
+
+func getValAddresses(vals []staking.Validator) []valAdd {
+	var ph sdk.Coin
+	var totalConsensusPower = getTotalTokens(vals)
+
+	for _, val := range vals {
+		accAdd, _ := deriveValAccAddress(val)
+		vP := fmt.Sprintf("%f", getVotingPower(val, totalConsensusPower))
+		valAdds = append(valAdds, valAdd{val.GetMoniker(), val.OperatorAddress, accAdd.String(), vP, ph.String(), val.BondedTokens().String() + "uatom"})
+	}
+
+	return valAdds
+}
+
+func getSelfDelegation(grpcConn *grpc.ClientConn, valAdds []valAdd,
+) error {
+
+	// This creates a gRPC client to query the x/staking service.
+	stakingClient := staking.NewQueryClient(grpcConn)
+
+	for _, val := range valAdds {
+		stakingRes4, _ := stakingClient.DelegatorDelegations(
+			context.Background(),
+			&staking.QueryDelegatorDelegationsRequest{DelegatorAddr: val.accAdd},
+		)
+
+		var delRes []staking.DelegationResponse = stakingRes4.DelegationResponses
+
+		for i, del := range delRes {
+			if del.Delegation.DelegatorAddress == val.accAdd {
+				valAdds[i].selfDelegation = del.Balance.String()
+			}
+		}
+	}
+	return nil
+}
+
+func exportCSV(valAdds []valAdd) {
+
+	file, err := os.Create("challenge_01.csv")
+	if err != nil {
+		log.Fatalln("failed to open file", err)
+	}
+	w := csv.NewWriter(file)
+	defer w.Flush()
+	// Using Write
+	for _, val := range valAdds {
+		row := []string{val.moniker, val.votingpower, val.selfDelegation, val.totalDelegation}
+		if err := w.Write(row); err != nil {
+			log.Fatalln("error writing record to file", err)
+		}
+	}
+	defer file.Close()
+}
